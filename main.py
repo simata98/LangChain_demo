069b644f088cebbb0b41bd5e6d56c297b8bc1411
simata98 <wlsrnr905@naver.com>
2023-10-26 14:27:36 +0900

    main.py 수정

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 chat_model = ChatOpenAI()
 
 st.title('KT 인공지능 서비스')
-st.title('_LLM_ 과 _LangChain_은 :blue[cool] :sunglasses:')
+st.title('_LLM_ 과 _LangChain_ 은 :blue[cool] :sunglasses:')
 
 content = st.text_input('시의 주제를 제시해주세요')
 st.write('입력 받은 시의 주제는', content)
